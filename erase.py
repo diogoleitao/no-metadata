0b1ce9406f67f51684a81eb42d0b6accbc775545
Diogo Leitão <diogo.n.leitao@gmail.com>
2015-10-28 23:35:24 +0000

    Project now reads /dev/sdaX raw as hex and quote-printable and prints the file size in GB

--- erase.py
+++ erase.py
@@ -1,4 +1,16 @@
+import binascii
+import os
+
 if __name__ == "__main__":
-    f = open("/dev/sda4", "r")
-    print f
-    f.close()
+    """dir_full_path = raw_input("Please enter the
+        full path for the directory to
+        have its metadata erased: ")"""
+
+    dir_full_path = "/dev/sda"
+    with open(dir_full_path, "r") as raw_image:
+        raw_image.seek(0, os.SEEK_END)
+        gbytes = raw_image.tell() / (1024 ** 3)
+        print gbytes
+        while True:
+            print binascii.b2a_qp(raw_image.read(4)),
+            raw_input()
