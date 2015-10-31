751b30527a1a683cdd4e944f255f345c445ffbe3
Diogo Leitão <diogo.n.leitao@gmail.com>
2015-10-31 19:46:45 +0000

    We can read the inode tables and have added some data to the stats file to keep track of the data needed to travel the raw OS file

--- erase.py
+++ erase.py
@@ -6,11 +6,17 @@ if __name__ == "__main__":
         full path for the directory to
         have its metadata erased: ")"""
 
-    dir_full_path = "/dev/sda"
+    superblock = 1024
+    block_group_descriptor_table = 4096
+    second_inode_table = 33710 * 4096
+
+    file_to_delete = 942 * 4096 + 3390 * 128
+
+    dir_full_path = "/dev/sdc1"
     with open(dir_full_path, "r") as raw_image:
-        raw_image.seek(0, os.SEEK_END)
-        gbytes = raw_image.tell() / (1024 ** 3)
-        print gbytes
+        raw_image.seek(file_to_delete)  # start of second inode table
+        # gbytes = raw_image.tell() / (1024 ** 3)
+        # print gbytes
         while True:
-            print binascii.b2a_qp(raw_image.read(4)),
+            print binascii.b2a_hex(raw_image.read(4)),
             raw_input()
