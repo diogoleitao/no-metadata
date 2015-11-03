7a978ad0c2dbded1d0e757a8d8c155f6b48738da
Diogo Leitão <diogo.n.leitao@gmail.com>
2015-11-03 23:50:13 +0000

    Added bit mask for directory checking on inodes

--- erase.py
+++ erase.py
@@ -21,6 +21,7 @@ if __name__ == "__main__":
     SUPERBLOCK = 1024
     ROOT_INODE_NO = 2
     INODE_SIZE = 128
+    DIR_MASK = 16384  # Mask for bit-by-bit check if inode is directory (0x4000)
 
     # STRUCTS
     sb_offsets = {
@@ -37,6 +38,7 @@ if __name__ == "__main__":
     }
 
     inode_offsets = {
+        "inode_mode": 0,
         "inode_first_data_block": 40
     }
 
@@ -54,6 +56,7 @@ if __name__ == "__main__":
     }
 
     inode_values = {
+        "inode_mode": 0,
         "inode_first_data_block": 0
     }
 
@@ -81,11 +84,13 @@ if __name__ == "__main__":
 
         root_inode = bgdt_values["bg_inode_table"] * sb_values["block_size"] + INODE_SIZE * ROOT_INODE_NO
 
+        raw_image.seek(root_inode + inode_offsets["inode_mode"])
+        inode_values["inode_mode"] = binascii.b2a_hex(raw_image.read(2))
+
         raw_image.seek(root_inode + inode_offsets["inode_first_data_block"])
         inode_values["inode_first_data_block"] = hexConverter(binascii.b2a_hex(raw_image.read(4)))
 
         raw_image.seek(inode_values["inode_first_data_block"] * sb_values["block_size"])
-
         while True:
             print binascii.b2a_qp(raw_image.read(4)),
             # print raw_image.tell(),
