014476a197aef23d28fae46aae546f459d990c59
Diogo Leitão <diogo.n.leitao@gmail.com>
2015-11-03 23:16:56 +0000

    Finding root directory process automated

--- erase.py
+++ erase.py
@@ -2,49 +2,91 @@ import binascii
 
 
 def hexConverter(byte):
-    reversed_hex = 0
-    reversed_hex_str = ""
+    reversed_hex_str = "0x"
     i = 0
-    # probably wrong
-    while i < byte.length:
-        reversed_hex_str += byte[byte.length - 2 - i]
-        reversed_hex_str += byte[byte.length - 1 - i]
+    while i < len(byte):
+        reversed_hex_str += byte[len(byte) - 2 - i]
+        reversed_hex_str += byte[len(byte) - 1 - i]
         i += 2
-    # convert to dec part missing
-    return reversed_hex
+    return int(reversed_hex_str, 0)
+
+
+def getDevSize():
+    gbytes = raw_image.tell() / (1024 ** 3)
+    print gbytes
 
 if __name__ == "__main__":
-    """dir_full_path = raw_input("Please enter the
-        full path for the directory to
-        have its metadata erased: ")"""
 
-    info_bytes = {
-        "inode_count": 4,
-        "block_count": 4,
-        "block_size": 4,
-        "blocks_per_group": 4,
-        "inodes_per_group": 4
+    # CONSTANTS
+    SUPERBLOCK = 1024
+    ROOT_INODE_NO = 2
+    INODE_SIZE = 128
+
+    # STRUCTS
+    sb_offsets = {
+        "block_size": 24,
+        "blocks_per_group": 32,
+        "inodes_per_group": 40
+        # "inode_count": 4,
+        # "block_count": 4,
     }
 
-    info_values = {
-        "inode_count": 0,
-        "block_count": 0,
+    bgdt_offsets = {
+        "inode_bitmap": 4,
+        "bg_inode_table": 8
+    }
+
+    inode_offsets = {
+        "inode_first_data_block": 40
+    }
+
+    sb_values = {
         "block_size": 0,
         "blocks_per_group": 0,
         "inodes_per_group": 0
+        # "inode_count": 0,
+        # "block_count": 0,
     }
 
-    superblock = 1024
-    block_group_descriptor_table = 4096
-    second_inode_table = 33710 * 4096
+    bgdt_values = {
+        "inode_bitmap": 0,
+        "bg_inode_table": 0
+    }
 
-    file_to_delete = 942 * 4096 + 3390 * 128
+    inode_values = {
+        "inode_first_data_block": 0
+    }
 
-    dir_full_path = "/dev/sdc1"
+    # VARIABLES
+    dir_full_path = "/dev/sdb1"
+    block_group_descriptor_table = 0
+    first_inode_table = 0 * sb_values["block_size"]
+    # second_inode_table = 33710 * info_values["block_size"]
+
     with open(dir_full_path, "r") as raw_image:
-        raw_image.seek(file_to_delete)  # start of second inode table
-        # gbytes = raw_image.tell() / (1024 ** 3)
-        # print gbytes
+        for field, offset in sb_offsets.iteritems():
+            raw_image.seek(SUPERBLOCK + offset)
+            sb_values[field] = hexConverter(binascii.b2a_hex(raw_image.read(4)))
+
+        sb_values["block_size"] = 1024 << sb_values["block_size"]
+
+        if sb_values["block_size"] > 1024:
+            block_group_descriptor_table = sb_values["block_size"]
+        else:
+            block_group_descriptor_table = sb_values["block_size"] * 2
+
+        for field, offset in bgdt_offsets.iteritems():
+            raw_image.seek(block_group_descriptor_table + offset)
+            bgdt_values[field] = hexConverter(binascii.b2a_hex(raw_image.read(4)))
+
+        root_inode = bgdt_values["bg_inode_table"] * sb_values["block_size"] + INODE_SIZE * ROOT_INODE_NO
+
+        raw_image.seek(root_inode + inode_offsets["inode_first_data_block"])
+        inode_values["inode_first_data_block"] = hexConverter(binascii.b2a_hex(raw_image.read(4)))
+
+        raw_image.seek(inode_values["inode_first_data_block"] * sb_values["block_size"])
+
         while True:
-            print binascii.b2a_hex(raw_image.read(4)),
+            print binascii.b2a_qp(raw_image.read(4)),
+            # print raw_image.tell(),
             raw_input()
