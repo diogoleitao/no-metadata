980fba5572eb7a37239a9752dbe0a6a5573daf90
Diogo Leitão <diogo.n.leitao@gmail.com>
2015-11-03 17:15:16 +0000

    Hex converter function started; dictionaries added for retrieving and storing information

--- erase.py
+++ erase.py
@@ -1,10 +1,38 @@
 import binascii
-import os
+
+
+def hexConverter(byte):
+    reversed_hex = 0
+    reversed_hex_str = ""
+    i = 0
+    # probably wrong
+    while i < byte.length:
+        reversed_hex_str += byte[byte.length - 2 - i]
+        reversed_hex_str += byte[byte.length - 1 - i]
+        i += 2
+    # convert to dec part missing
+    return reversed_hex
 
 if __name__ == "__main__":
     """dir_full_path = raw_input("Please enter the
         full path for the directory to
         have its metadata erased: ")"""
+
+    info_bytes = {
+        "inode_count": 4,
+        "block_count": 4,
+        "block_size": 4,
+        "blocks_per_group": 4,
+        "inodes_per_group": 4
+    }
+
+    info_values = {
+        "inode_count": 0,
+        "block_count": 0,
+        "block_size": 0,
+        "blocks_per_group": 0,
+        "inodes_per_group": 0
+    }
 
     superblock = 1024
     block_group_descriptor_table = 4096
